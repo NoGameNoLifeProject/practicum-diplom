654aae757ce5eacb5046a8e83abce29beddada13
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2025-04-16 18:47:13 +0300

    Убрал список т.к. не понял как реализовать маппер для этого. Думаю нужно использовать другую сущность для списка

--- app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class AreaDto(
-    val areas: ArrayList<AreaDto> = arrayListOf(),
+//    val areas: ArrayList<AreaDto> = arrayListOf(),  !это надо менять
     val id: String? = null,
     val name: String? = null,
     @SerializedName("parent_id")
