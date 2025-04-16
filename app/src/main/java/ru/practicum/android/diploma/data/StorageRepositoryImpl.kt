ef222e7f9cd9ae84cc480e891af16f8056784145
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2025-04-16 18:45:39 +0300

    Переписал Storage т.к. он нам по идее нужен для параметров фильтрации

--- app/src/main/java/ru/practicum/android/diploma/data/StorageRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/StorageRepositoryImpl.kt
@@ -1,22 +1,26 @@
 package ru.practicum.android.diploma.data
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.google.gson.Gson
-import ru.practicum.android.diploma.data.dto.VacancyDto
 import ru.practicum.android.diploma.domain.api.IStorageRepository
+import ru.practicum.android.diploma.domain.models.SearchVacanciesParam
 
-class StorageRepositoryImpl(private val storageSharedPreferences: SharedPreferences) :
+class StorageRepositoryImpl(
+    private val storageSharedPreferences: SharedPreferences,
+    private val gson: Gson
+) :
     IStorageRepository {
 
-    override fun read(): List<VacancyDto> {
+    override fun read(): SearchVacanciesParam {
         val json = storageSharedPreferences.getString(STORAGE_PREFERENCES_KEY, null)
-            ?: return arrayListOf()
-        return ArrayList(Gson().fromJson(json, Array<VacancyDto>::class.java).toList())
+            ?: return SearchVacanciesParam()
+        return gson.fromJson(json, SearchVacanciesParam::class.java)
     }
 
-    override fun write(vacancies: List<VacancyDto>) {
-        val json = Gson().toJson(vacancies)
-        storageSharedPreferences.edit().putString(STORAGE_PREFERENCES_KEY, json).apply()
+    override fun write(filterParam: SearchVacanciesParam) {
+        val json = gson.toJson(filterParam)
+        storageSharedPreferences.edit() { putString(STORAGE_PREFERENCES_KEY, json) }
     }
 
     companion object {
