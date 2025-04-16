77eed889ffeec48a62097092cdee69bd15536863
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2025-04-16 18:53:35 +0300

    Убрал flow т.к. по идее у нас flow вызывается в методах репозитория

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/VacancyInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/VacancyInteractorImpl.kt
@@ -1,37 +1,35 @@
 package ru.practicum.android.diploma.domain.impl
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import ru.practicum.android.diploma.data.dto.AreaDto
-import ru.practicum.android.diploma.data.dto.IndustryDto
-import ru.practicum.android.diploma.data.dto.VacancyDto
-import ru.practicum.android.diploma.data.dto.VacancyDetailsDto
-import ru.practicum.android.diploma.data.network.dto.GetVacancyDetailsRequest
-import ru.practicum.android.diploma.data.network.dto.SearchVacanciesRequest
+import kotlinx.coroutines.flow.map
 import ru.practicum.android.diploma.domain.api.IVacancyInteractor
 import ru.practicum.android.diploma.domain.api.IVacancyRepository
 import ru.practicum.android.diploma.domain.api.Resource
+import ru.practicum.android.diploma.domain.models.Area
+import ru.practicum.android.diploma.domain.models.Industry
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.VacancyDetails
 
 class VacancyInteractorImpl(private val repository: IVacancyRepository) : IVacancyInteractor {
-    override fun searchVacancies(expression: String): Flow<Pair<List<VacancyDto>?, String?>> = flow {
-        repository.searchVacancies(SearchVacanciesRequest(text = expression, page = 0)).collect { result ->
+    override fun searchVacancies(expression: String): Flow<Pair<List<Vacancy>?, String?>> {
+        return repository.searchVacancies(expression).map { result ->
             when (result) {
                 is Resource.Error -> {
-                    emit(Pair(null, result.message))
+                    Pair(null, result.message)
                 }
 
                 is Resource.Success -> {
-                    emit(Pair(result.data?.items?.toList(), null))
+                    Pair(result.data?.items?.toList(), null)
                 }
             }
         }
     }
 
-    override fun getCountries(): Flow<Pair<List<AreaDto>?, String?>> = flow {
-        repository.getCountries().collect() { result ->
+    override fun getCountries(): Flow<Pair<List<Area>?, String?>> {
+        return repository.getCountries().map { result ->
             when (result) {
                 is Resource.Error -> {
-                    emit(Pair(null, result.message))
+                    Pair(null, result.message)
                 }
 
                 is Resource.Success -> {
@@ -41,32 +39,32 @@ class VacancyInteractorImpl(private val repository: IVacancyRepository) : IVacan
         }
     }
 
-    override fun getIndustries(): Flow<Pair<List<IndustryDto>?, String?>> = flow {
-        repository.getIndustries().collect { result ->
+    override fun getIndustries(): Flow<Pair<List<Industry>?, String?>> {
+        return repository.getIndustries().map { result ->
             when (result) {
                 is Resource.Error -> {
-                    emit(Pair(null, result.message))
+                    Pair(null, result.message)
                 }
 
                 is Resource.Success -> {
-                    emit(Pair(result.data, null))
+                    Pair(result.data, null)
                 }
             }
         }
     }
 
-    override fun getVacancyDetails(vacancyId: String): Flow<Pair<VacancyDetailsDto?, String?>> = flow {
-        repository.getVacancyDetails(GetVacancyDetailsRequest(vacancyId)).collect { result ->
+    override fun getVacancyDetails(vacancyId: String): Flow<Pair<VacancyDetails?, String?>> {
+        return repository.getVacancyDetails(vacancyId).map { result ->
             when (result) {
                 is Resource.Error -> {
-                    emit(Pair(null, result.message))
+                    Pair(null, result.message)
                 }
 
                 is Resource.Success -> {
                     if (result.data == null) {
-                        emit(Pair(null, result.message))
+                        Pair(null, result.message)
                     } else {
-                        emit(Pair(result.data, null))
+                        Pair(result.data, null)
                     }
                 }
             }
