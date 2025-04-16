d053fcf134c0d5c99a9fee0ba9b796b000e21ea0
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2025-04-16 18:59:11 +0300

    Этот класс полностью копирует VacancyDetailsDto, стоит подумать над его удалением.

--- app/src/main/java/ru/practicum/android/diploma/data/network/dto/GetVacancyDetailsResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/dto/GetVacancyDetailsResponse.kt
@@ -1,26 +1,5 @@
 package ru.practicum.android.diploma.data.network.dto
 
-import com.google.gson.annotations.SerializedName
-import ru.practicum.android.diploma.data.dto.AreaDto
-import ru.practicum.android.diploma.data.dto.EmployerDto
-import ru.practicum.android.diploma.data.dto.ExperienceDto
-import ru.practicum.android.diploma.data.dto.KeySkillDto
-import ru.practicum.android.diploma.data.dto.SalaryDto
+import ru.practicum.android.diploma.data.dto.VacancyDetailsDto
 
-data class GetVacancyDetailsResponse(
-    val id: String,
-    val name: String,
-
-    val area: AreaDto? = AreaDto(),
-
-    val description: String? = null,
-
-    val employer: EmployerDto? = null,
-
-    @SerializedName("key_skills") val keySkills: ArrayList<KeySkillDto> = arrayListOf(),
-
-    val salary: SalaryDto? = null,
-    val salaryRange: SalaryDto? = null,
-
-    val experience: ExperienceDto? = null,
-)
+data class GetVacancyDetailsResponse(val vacancyDetails: VacancyDetailsDto)
