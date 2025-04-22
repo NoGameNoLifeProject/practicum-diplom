9a4cf2e263a91b3009be1ff315fbfa8919c90ec5
LuxuriousGerg <georggalileo21@mail.ru>
2025-04-22 17:23:57 +0300

    detekt fix

--- app/src/main/java/ru/practicum/android/diploma/data/NetworkInfoDataSourceImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/NetworkInfoDataSourceImpl.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
-class NetworkInfoDataSourceImpl(
-    private val context: Context
-) : NetworkInfoDataSource {
+class NetworkInfoDataSourceImpl(private val context: Context) : NetworkInfoDataSource {
 
-    private val cm =
-        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     override fun isConnected(): Boolean {
         val network = cm.activeNetwork ?: return false
-        val caps    = cm.getNetworkCapabilities(network) ?: return false
-        return caps.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        return cm.getNetworkCapabilities(network)
+            ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            ?: false
     }
 }
