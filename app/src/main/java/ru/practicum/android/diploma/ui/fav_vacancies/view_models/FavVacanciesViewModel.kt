ed82d490b92df0543729f82765cd4ebc47058eff
Kira <34008081+NoGameNoLifeProject@users.noreply.github.com>
2025-04-23 00:21:43 +0300

    ViewModel экрана Избранное

--- app/src/main/java/ru/practicum/android/diploma/ui/fav_vacancies/view_models/FavVacanciesViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fav_vacancies/view_models/FavVacanciesViewModel.kt
@@ -1,7 +1,44 @@
 package ru.practicum.android.diploma.ui.fav_vacancies.view_models
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.api.IFavVacanciesInteractor
+import ru.practicum.android.diploma.domain.api.Resource
+import ru.practicum.android.diploma.domain.models.FavVacanciesState
 
-class FavVacanciesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class FavVacanciesViewModel(private val favVacanciesInteractor: IFavVacanciesInteractor) : ViewModel() {
+    private val _state = MutableLiveData<FavVacanciesState>()
+    val state: LiveData<FavVacanciesState> get() = _state
+
+    private fun renderState(state: FavVacanciesState) {
+        _state.postValue(state)
+    }
+
+    init {
+        getFavVacancies()
+    }
+
+    private fun getFavVacancies() {
+        renderState(FavVacanciesState.Loading)
+        viewModelScope.launch {
+            favVacanciesInteractor.getFavorite().collect { result ->
+                when (result) {
+                    is Resource.Success -> {
+                        if (result.data.isNullOrEmpty()) {
+                            renderState(FavVacanciesState.Empty)
+                        } else {
+                            renderState(FavVacanciesState.VacanciesList(result.data))
+                        }
+                    }
+                    is Resource.Error -> {
+                        renderState(FavVacanciesState.Error)
+                    }
+                }
+            }
+        }
+    }
+
 }
