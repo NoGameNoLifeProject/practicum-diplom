a36943ff13f64c0ada6996b5e91e67114f71cf5a
AlexandrFedoseev-1 <MrHumanFed@yandex.ru>
2025-04-22 13:40:37 +0300

    detekt fix

--- app/src/main/java/ru/practicum/android/diploma/ui/common/HHToolbar.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/HHToolbar.kt
@@ -12,26 +12,31 @@ import androidx.core.view.isVisible
 import ru.practicum.android.diploma.R
 
 /**
-* Основан на LinearLayout с заголовком и тремя кнопками
-*
-* Для обработки нажатий можно установить обработчики
+ * Основан на LinearLayout с заголовком и тремя кнопками
+ *
+ * Для обработки нажатий можно установить обработчики
  * [setOnNavigationClick], [setOnAction1Click], [setOnAction2Click]
-*
+ *
  * Для изменения параметров используются поля и соответствующие атрибуты в xml
-*
-* @property drawableNavigation
-* @property drawableAction1
-* @property drawableAction2 Action1 и Action2 на экране нумеруются справа налево
-* @property titleText
-*
-**/
-class HHToolbar: LinearLayout {
+ *
+ * @property drawableNavigation
+ * @property drawableAction1
+ * @property drawableAction2 Action1 и Action2 на экране нумеруются справа налево
+ * @property titleText
+ *
+ **/
+class HHToolbar : LinearLayout {
 
-    constructor(context: Context): this(context, null)
-    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    ) {
 
         LayoutInflater.from(context)
             .inflate(R.layout.view_toolbar, this, true)
@@ -42,30 +47,33 @@ class HHToolbar: LinearLayout {
         action1Img = findViewById(R.id.toolbarImgAction1)
 
         val params = context.obtainStyledAttributes(
-            attrs, R.styleable.HHToolbar, defStyleAttr, defStyleRes
+            attrs,
+            R.styleable.HHToolbar,
+            defStyleAttr,
+            defStyleRes
         )
 
         drawableNavigation = params.getDrawable(R.styleable.HHToolbar_drawableNavigation)
         drawableAction1 = params.getDrawable(R.styleable.HHToolbar_drawableAction1)
         drawableAction2 = params.getDrawable(R.styleable.HHToolbar_drawableAction2)
-        titleText = params.getString(R.styleable.HHToolbar_titleText)?:""
+        titleText = params.getString(R.styleable.HHToolbar_titleText) ?: ""
 
         params.recycle()
     }
 
     private val navigationImg: ImageView
     private val titleTextView: TextView
-    private val action2Img:ImageView
-    private val action1Img:ImageView
+    private val action2Img: ImageView
+    private val action1Img: ImageView
 
     var drawableNavigation: Drawable? = null
         set(value) {
             field = value
-            if (value == null)  {
+            if (value == null) {
                 val leftPaddingPx = resources.getDimension(R.dimen.toolbar_no_nav_padding).toInt()
-                titleTextView.setPadding(leftPaddingPx, titleTextView.paddingTop,0,titleTextView.paddingBottom)
+                titleTextView.setPadding(leftPaddingPx, titleTextView.paddingTop, 0, titleTextView.paddingBottom)
             } else {
-                titleTextView.setPadding(0, titleTextView.paddingTop,0,titleTextView.paddingBottom)
+                titleTextView.setPadding(0, titleTextView.paddingTop, 0, titleTextView.paddingBottom)
             }
             updateDrawable(navigationImg, value)
         }
@@ -89,13 +97,15 @@ class HHToolbar: LinearLayout {
             redrawItems()
         }
 
-    fun setOnNavigationClick(onClick: (()->Unit)? = null) {
+    fun setOnNavigationClick(onClick: (() -> Unit)? = null) {
         updateOnClick(navigationImg, onClick)
     }
-    fun setOnAction1Click(onClick: (()->Unit)? = null) {
+
+    fun setOnAction1Click(onClick: (() -> Unit)? = null) {
         updateOnClick(action1Img, onClick)
     }
-    fun setOnAction2Click(onClick: (()->Unit)? = null) {
+
+    fun setOnAction2Click(onClick: (() -> Unit)? = null) {
         updateOnClick(action2Img, onClick)
     }
 
@@ -109,7 +119,7 @@ class HHToolbar: LinearLayout {
         redrawItems()
     }
 
-    private fun updateOnClick(view: View, onClick: (()->Unit)?) {
+    private fun updateOnClick(view: View, onClick: (() -> Unit)?) {
         if (onClick == null) {
             view.setOnClickListener(null)
         } else {
