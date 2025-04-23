ec47458458bb94774c88e2bfde39c96bccb1214f
DariaBan <dariabandukova@gmail.com>
2025-04-23 10:48:40 +0300

    Все что смогла сделать пока

--- app/src/main/java/ru/practicum/android/diploma/ui/search_vacancies/adapter/VacancyAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search_vacancies/adapter/VacancyAdapter.kt
@@ -1,12 +1,58 @@
 package ru.practicum.android.diploma.ui.search_vacancies.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.models.Vacancy
 
-class VacancyAdapter(private var vacancyList: List<Vacancy>, private val clickListener: (Vacancy) -> Unit) :
+class VacancyAdapter(
+    private var vacancyList: List<Vacancy>,
+    private val clickListener: (Vacancy) -> Unit
+) :
     RecyclerView.Adapter<VacancyAdapter.VacancyViewHolder>() {
 
-    class VacancyViewHolder(parentView: View) : RecyclerView.ViewHolder(parentView)
+    class VacancyViewHolder(parentView: View) : RecyclerView.ViewHolder(parentView) {
+        private val vacancyName: TextView = parentView.findViewById(R.id.vacancy_name_card)
+        private val industryName: TextView = parentView.findViewById(R.id.vacancy_industry_card)
+        private val salary: TextView = parentView.findViewById(R.id.vacancy_salary_card)
+        private val vacancyImage: ImageView = parentView.findViewById(R.id.vacancy_card_placeholder)
 
+        fun bind(vacancy: Vacancy) {
+            vacancyName.text = vacancy.name
+            industryName.text = vacancy.employer?.name
+            salary.text = vacancy.salary?.currency
+            Glide.with(vacancyImage)
+                .load(vacancy.employer?.logoUrls?.size90)
+                .placeholder(R.drawable.ic_placeholder_32px)
+                .transform(RoundedCorners(10))
+                .submit()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacancyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.vacancy_item, parent, false)
+        return VacancyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return vacancyList.size
+    }
+
+    override fun onBindViewHolder(holder: VacancyViewHolder, position: Int) {
+        holder.bind(vacancyList[position])
+        holder.itemView.setOnClickListener {
+            clickListener.invoke(vacancyList[position])
+        }
+    }
+
+    fun updateVacancy(newVacancy: List<Vacancy>) {
+        vacancyList = newVacancy
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/search_vacancies/fragments/SearchVacanciesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search_vacancies/fragments/SearchVacanciesFragment.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentSearchVacanciesBinding
+import ru.practicum.android.diploma.domain.models.SearchVacanciesState
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.ui.common.SearchBarView
+import ru.practicum.android.diploma.ui.search_vacancies.adapter.VacancyAdapter
 import ru.practicum.android.diploma.ui.search_vacancies.view_models.SearchVacanciesViewModel
 
 class SearchVacanciesFragment : Fragment() {
@@ -16,11 +24,12 @@ class SearchVacanciesFragment : Fragment() {
     }
 
     private val viewModel: SearchVacanciesViewModel by viewModel<SearchVacanciesViewModel>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+    private lateinit var binding: FragmentSearchVacanciesBinding
+    private val vacancies = mutableListOf<Vacancy>()
+    private val searchBar = SearchBarView(requireContext())
+    private var input: String? = null
+    private val adapter = VacancyAdapter(vacancies) {
+        findNavController().navigate(R.id.action_searchVacanciesFragment_to_vacancyDetailsFragment)
     }
 
     override fun onCreateView(
@@ -30,4 +39,85 @@ class SearchVacanciesFragment : Fragment() {
     ): View {
         return inflater.inflate(R.layout.fragment_search_vacancies, container, false)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupToolBar()
+        setupRecyclerViews()
+        viewModel.observeState().observe(viewLifecycleOwner) { render(it) }
+        input = searchBar.getQuery()
+        searchBar.setOnQueryTextChangedListener { viewModel.searchDebounce(input!!) }
+        binding.filterButton.setOnClickListener {
+            binding.filterButton.setImageResource(R.drawable.ic_filter_on_24px)
+            findNavController().navigate(R.id.action_searchVacanciesFragment_to_filterParametersFragment)
+        }
+        binding.foundedVacancy.text =
+            String.format(getString(R.string.search_vacancies_chip_found_vacancies), vacancies.size.toString())
+    }
+
+    private fun setupToolBar() {
+        binding.searchToolBar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun setupRecyclerViews() {
+        binding.recyclerViewVacancy.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewVacancy.adapter = adapter
+    }
+
+    private fun render(state: SearchVacanciesState) {
+        when (state) {
+            is SearchVacanciesState.VacanciesList -> {
+                adapter.updateVacancy(state.vacancies)
+                showVacancies()
+            }
+
+            is SearchVacanciesState.Loading -> showLoadingPage()
+            is SearchVacanciesState.Empty -> showEmpty()
+            is SearchVacanciesState.NetworkError -> showNetworkError()
+            is SearchVacanciesState.NothingFound -> showNothingFound()
+        }
+    }
+
+    private fun showVacancies() {
+        binding.recyclerViewVacancy.isVisible = true
+        binding.foundedVacancy.isVisible = true
+        binding.placeholderLayout.isVisible = false
+        binding.progressCircular.isVisible = false
+    }
+
+    private fun showLoadingPage() {
+        binding.recyclerViewVacancy.isVisible = false
+        binding.foundedVacancy.isVisible = false
+        binding.placeholderLayout.isVisible = false
+        binding.progressCircular.isVisible = true
+    }
+
+    private fun showEmpty() {
+        binding.recyclerViewVacancy.isVisible = false
+        binding.foundedVacancy.isVisible = false
+        binding.placeholderLayout.isVisible = true
+        binding.vacancyTextPlaceholder.isVisible = false
+        binding.progressCircular.isVisible = false
+    }
+
+    private fun showNetworkError() {
+        binding.recyclerViewVacancy.isVisible = false
+        binding.foundedVacancy.isVisible = false
+        binding.placeholderLayout.isVisible = true
+        binding.vacancyImagePlaceholder.setImageResource(R.drawable.image_error_no_internet)
+        binding.vacancyTextPlaceholder.setText(R.string.search_vacancies_no_internet)
+        binding.progressCircular.isVisible = false
+    }
+
+    private fun showNothingFound() {
+        binding.recyclerViewVacancy.isVisible = false
+        binding.foundedVacancy.isVisible = true
+        binding.placeholderLayout.isVisible = true
+        binding.vacancyImagePlaceholder.setImageResource(R.drawable.image_error_404)
+        binding.vacancyTextPlaceholder.setText(R.string.search_vacancies_placeholder_not_found)
+        binding.progressCircular.isVisible = false
+    }
+
 }
